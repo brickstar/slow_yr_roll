2014eaca1613b19f393a8ee451b39458facfba75
brickstar <brickstar@gmail.com>
2018-09-08 17:25:40 -0600

    playing with current user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,11 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    @current_user ||= User.find(session[:id]) if session[:id]
+    if session[:user_id]
+      id = session[:user_id]
+      if User.exists?(id)
+        @current_user ||= User.find(id)
+      end
+    end
   end
 end
