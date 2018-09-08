51a95ecfadfbef33c0487c7cb1222a277f5747db
brickstar <brickstar@gmail.com>
2018-09-08 11:06:55 -0600

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :songs
+  root to: 'welcome#index'
+  get '/stream', to: 'songss#stream', as: :stream
 end
