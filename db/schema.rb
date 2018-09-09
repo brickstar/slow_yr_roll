f2a41dc6508680d243d595121cf7ca13d5a5eb9f
brickstar <brickstar@gmail.com>
2018-09-08 20:21:50 -0600

    add songs

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_08_225219) do
+ActiveRecord::Schema.define(version: 2018_09_09_021838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "songs", force: :cascade do |t|
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_songs_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider"
@@ -28,4 +35,5 @@ ActiveRecord::Schema.define(version: 2018_09_08_225219) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "songs", "users"
 end
