ea23f845861023f7cc332034c42a9c1991b76427
brickstar <brickstar@gmail.com>
2018-09-10 13:53:18 -0600

    setting up with s3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :songs
-  root to: 'welcome#index'
+  root to: 'songs#index'
   post "songs/upload", :as => "upload"
   delete "songs/delete", :as => "delete"
   get '/auth/:provider/callback', to: 'sessions#create'
