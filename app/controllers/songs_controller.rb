0b72e3b40addf73d481a242033f4ddfdfc530640
brickstar <brickstar@gmail.com>
2018-09-14 21:30:18 -0600

    remove pry

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -12,7 +12,6 @@ class SongsController < ApplicationController
     @song = current_user.songs.create!(song_params)
     if @song.save
       flash[:notice] = "Successfully added new song!"
-      binding.pry
       redirect_to songs_path
     else
       flash[:alert] = "Error adding new song!"
