7f5ec7af46483884aa415abaeccf7606187aa689
brickstar <brickstar@gmail.com>
2018-09-15 18:11:13 -0600

    update songs controller for new user workflow

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,14 +5,20 @@ class SongsController < ApplicationController
     @songs = Song.all
   end
 
+  def new
+    @song = current_user.songs.new
+  end
+
   def show
+    @song = current_user.songs.new
+    @songs = Song.all
   end
 
   def create
     @song = current_user.songs.create!(song_params)
     if @song.save
       flash[:notice] = "Successfully added new song!"
-      redirect_to songs_path
+      redirect_to song_path(@song)
     else
       flash[:alert] = "Error adding new song!"
       render :new
