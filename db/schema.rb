dc6e4eac76582f571457d669ce86253087a5bf61
brickstar <brickstar@gmail.com>
2018-09-15 18:11:33 -0600

    add title column to songs table

--- db/schema.rb
+++ db/schema.rb
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 2018_09_09_175409) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "data"
+    t.string "title"
     t.index ["user_id"], name: "index_songs_on_user_id"
   end
 
