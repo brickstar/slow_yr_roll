f8f18ffd82cd920f004cc967bca76048a99891cc
brickstar <brickstar@gmail.com>
2018-09-16 20:01:46 -0600

    cleanup

--- spec/features/user_adds_song_spec.rb
+++ spec/features/user_adds_song_spec.rb
@@ -8,14 +8,14 @@ describe 'logged in user' do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
   end
 
-  it 'can upload and  create a song' do
+  it 'can upload and create a song' do
 
     visit song_path(@song1)
 
     expect(Song.all.count).to eq(2)
 
     song3 = attach_file('song_audio', "#{Rails.root}/spec/features/test_uploads/beep_it.mp3", visible: false)
-    
+
     fill_in 'song[title]', with: 'new title'
 
     click_button 'Add Song'
